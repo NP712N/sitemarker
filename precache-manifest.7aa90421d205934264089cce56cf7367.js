cbba89b36fe8e3228be8bd5372a501763749b483
NP712N <npn19998@gmail.com>
2020-03-08 19:48:21 +0700

    Updates

---
+++ precache-manifest.7aa90421d205934264089cce56cf7367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed793b68141e681af237d3e6c2e36229",
+    "url": "/sitemarker/index.html"
+  },
+  {
+    "revision": "096349ca8fd51f355346",
+    "url": "/sitemarker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec71aa9ec5335590d38b",
+    "url": "/sitemarker/static/js/2.4695f3e9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sitemarker/static/js/2.4695f3e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096349ca8fd51f355346",
+    "url": "/sitemarker/static/js/main.f240f10f.chunk.js"
+  },
+  {
+    "revision": "893e4e4c5515de626d0f",
+    "url": "/sitemarker/static/js/runtime-main.845b8c96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sitemarker/static/media/logo.5d5d9eef.svg"
+  }
+]);
